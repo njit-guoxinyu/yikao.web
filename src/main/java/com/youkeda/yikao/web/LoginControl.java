02f7cbbd726f050845ba343835064d0a95d9bb0e
郭鑫宇 <827155369@qq.com>
2019-03-27 23:52:09 +0800

    v2

--- src/main/java/com/youkeda/yikao/web/LoginControl.java
+++ src/main/java/com/youkeda/yikao/web/LoginControl.java
@@ -1,7 +1,22 @@
 package com.youkeda.yikao.web;
 
+import com.youkeda.yikao.web.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class LoginControl {
 
+    @RequestMapping(path = "login")
+    public ModelAndView login(User user, HttpSession session){
 
-    
+        if (user.getName().equals("admin@admin.com")&& user.getPwd().equals("Bmatch123")){
+            session.setAttribute("user", user);
+            return new ModelAndView("redirect:index.html");
+        }
+            return new ModelAndView("redirect:login.html");
+    }
 }
